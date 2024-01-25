768a8adb5d66c2026f105c550377f9c25aaec0c0
ScorpN <sf_commander@outlook.com>
2024-01-25 14:27:45 +0300

    NextJS Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { inter } from '../app/ui/fonts'
+import '@/app/ui/global.css'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <head>
+        <title>TestCase</title>
+        <link href='https://www.mecellem.com/assets/icons/icon.css?4cb18f1beddb66a33174sda' rel='stylesheet'></link>
+      </head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
